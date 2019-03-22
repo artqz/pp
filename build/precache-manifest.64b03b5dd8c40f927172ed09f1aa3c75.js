fbaa420a7c2f9a9a228ff54ae09da61b8ae3d7d2
artqz <djoctuk@yandex.ru>
2019-03-22 23:25:20 +0500

    build

---
+++ build/precache-manifest.64b03b5dd8c40f927172ed09f1aa3c75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9d44743ba7acb5c59f19",
+    "url": "/static/js/main.9d44743b.chunk.js"
+  },
+  {
+    "revision": "1865a972172e40604fd0",
+    "url": "/static/js/2.1865a972.chunk.js"
+  },
+  {
+    "revision": "9d44743ba7acb5c59f19",
+    "url": "/static/css/main.c9031ba9.chunk.css"
+  },
+  {
+    "revision": "1865a972172e40604fd0",
+    "url": "/static/css/2.29130aec.chunk.css"
+  },
+  {
+    "revision": "519316edd145acfef911248f0c2ff768",
+    "url": "/index.html"
+  }
+];
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import ToggleTheme from './ToggleTheme'
+import ToggleTheme from './ToggleTheme';
 
 import './NavBar.css';
 
 function NavBar() {
   const ydClientId = '7f12f06a9a954a5d8d16c1d99cae62fa';
-  const ydRedirectUri = 'http://localhost:3000/yandex/callback';
-  const dbRedirectUri = 'http://localhost:3000/dropbox/callback';    
-  
+  const ydRedirectUri = 'http://134.0.116.172/yandex/callback';
+  const dbRedirectUri = 'http://134.0.116.172/dropbox/callback';
+
   const yd_token = localStorage.getItem('yd_access_token');
   const db_token = localStorage.getItem('db_access_token');
   return (
     <div className="navbar">
-    <div className="logo"><span className="first_letter">D</span>rop<span className="first_letter">P</span>rop<span className="first_letter">P</span>lay</div>
+      <div className="logo">
+        <span className="first_letter">D</span>rop
+        <span className="first_letter">P</span>rop
+        <span className="first_letter">P</span>lay
+      </div>
       {/*!yd_token ? <a href={`https://oauth.yandex.ru/authorize?response_type=token&client_id=${ydClientId}&redirect_uri=${ydRedirectUri}`}>Яндекс</a> : null*/}
-      {!db_token ? <a href={`https://www.dropbox.com/oauth2/authorize?client_id=3sf5dug4qbt4hbf&response_type=token&redirect_uri=${dbRedirectUri}`}>Дропбокс</a> : null}
+      {!db_token ? (
+        <a
+          href={`https://www.dropbox.com/oauth2/authorize?client_id=3sf5dug4qbt4hbf&response_type=token&redirect_uri=${dbRedirectUri}`}
+        >
+          Дропбокс
+        </a>
+      ) : null}
       <ToggleTheme />
     </div>
   );
