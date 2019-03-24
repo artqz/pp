e190259ce775c2e2d7b3522eb9408cc58b7a8496
artqz <djoctuk@yandex.ru>
2019-03-25 00:22:22 +0500

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,46 +1,48 @@
 import React, { useContext, useEffect } from 'react';
-import { search_mp3 } from '../Providers';
+import { search_mp3, db_connect } from '../Providers';
 import { AudioContext } from '../context/AudioContext';
 import TrackList from '../components/trackList/TrackList';
 import Audio from '../components/audio/Audio';
 
-function Home () { 
-  const {state, dispatch} = useContext(AudioContext);
+function Home() {
+  const { state, dispatch } = useContext(AudioContext);
 
   useEffect(() => {
+    db_connect().then(res => {
+      console.log(res);
+    });
     console.log('Player status:');
     if (localStorage.getItem('db_access_token')) {
       console.log('- Dropbox token found!');
       const itemsList = [];
-      console.log('- Start searching for tracks...');      
+      console.log('- Start searching for tracks...');
       search_mp3(localStorage.getItem('db_access_token')).then(res => {
-        res.data.matches.map(item => {      
-          if (item.metadata['.tag'] === 'file') {   
+        res.data.matches.map(item => {
+          if (item.metadata['.tag'] === 'file') {
             itemsList.push({
-              name: item.metadata.name, 
-              path: item.metadata.path_display, 
+              name: item.metadata.name,
+              path: item.metadata.path_display,
               provider: 'dropbox',
               playing: false
-            });           
+            });
           }
           return itemsList;
-        });   
-        console.log('- Found ' + itemsList.length + ' tracks!'); 
+        });
+        console.log('- Found ' + itemsList.length + ' tracks!');
         dispatch({
           type: 'SET_LIST',
           list: itemsList
-        });      
+        });
       });
     }
-  }, []);   
-  
+  }, []);
+
   return state.list[state.currentTrack] ? (
     <div>
-      <TrackList list={state.list} />      
+      <TrackList list={state.list} />
       <Audio track={state.list[state.currentTrack].path} />
     </div>
   ) : null;
-  
 }
 
 export default Home;
